e938ae5f6f8ed341c731b78b67eb9a0afb0711d4
xenohedron <xenohedron@users.noreply.github.com>
2017-06-17 15:34:59 -0400

    Comments in Card.java
    May as well make a new branch

--- Card.java
+++ Card.java
@@ -3,6 +3,11 @@ public class Card {
 
 	public Card() {
 		// TODO Auto-generated constructor stub
+		// Hey cool I can edit directly from GitHub
+		// Also you haven't written any code yet
+		// Which would be okay, except that you also
+		//	haven't published any sort of roadmap...
+		// So I don't have the slightest idea what to do
 	}
 
 }
